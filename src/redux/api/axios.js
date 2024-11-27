a577b71af3c8153573285a0d2de028e7d00e9b08
azimjonking <azimjonjalilov1301@mail.ru>
2024-11-27 17:59:31 +0200

    updated api link

--- src/redux/api/axios.js
+++ src/redux/api/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export const MAINURL = "https://algbot-production.up.railway.app";
+// export const MAINURL = "https://algbot-production.up.railway.app";
+export const MAINURL = "https://algoritmbot-production.up.railway.app";
 
 const axiosInstance = axios.create({
   baseURL: MAINURL,
